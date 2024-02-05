abdaf5d09727324bdc66705c779867a8d42698b8
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-05 11:11:07 -0300

    add first file

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main (String[] args) {
+		int value1 = 90, value2, value3; // declarando value2 e value3
+		double val1 = 1.98, val2, val3 = 34.786;
+		String name = "Guilherme";
+		System.out.println("Hello world");
+	}
+}
